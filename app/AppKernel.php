b9be19ca9b3c361293a4a8120c0a0e0cf016b449
liuqingjie <liuqingjie06@yahoo.com.cn>
2013-04-09 23:39:39 +0800

    增加了Acme MainBundle 作为主页
    Change-Id: I6b0a5838cda807f6e701e88859b706be2d10af7c

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,8 @@ class AppKernel extends Kernel
         	new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
         	new Vich\UploaderBundle\VichUploaderBundle(),
             new Acme\TryBundle\AcmeTryBundle(),
+            new Acme\AdminBundle\AcmeAdminBundle(),
+            new Acme\MainBundle\AcmeMainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
