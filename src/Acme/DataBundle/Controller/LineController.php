da6c5ad7cfbd81f2b02da94e3643667d2857a3d1
liuqingjie <liuqingjie06@yahoo.com.cn>
2013-04-17 02:06:27 +0800

    增加了线路添加查看的页面
    Change-Id: If7969b64e0b6abd84ca53ba1ddbe0fcf68092b0b

--- src/Acme/DataBundle/Controller/LineController.php
+++ src/Acme/DataBundle/Controller/LineController.php
@@ -1,7 +1,13 @@
 <?php
 
 namespace Acme\DataBundle\Controller;
-
+/**
+ * (c) liuqingjie06 <liuqingjie06@yahoo.com.cn>
+ * @author 刘庆杰
+ * date 2013-4-10
+ *
+ * 本controller用于线路信息的管理
+ */
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -32,11 +38,15 @@ class LineController extends Controller
 	public function createAction(Request $request)
 	{
 		$line = new Line();
+		$em = $this->getDoctrine()->getEntityManager();
 		if($request->isMethod('POST')){
+			$line->setName($_POST["name"]);
+			$em->persist($line);
+			$em->flush();
 			return $this->redirect($this->generateUrl('line'));
 		}
 			
-		return $this->redirect($this->generateUrl('line'));
+		return array();
 	}
 	
 	
